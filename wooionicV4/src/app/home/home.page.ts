6c8c775840f0a9cf183c9c7716c0c06c33f548ff
maria <debawimaria.fsm@gmail.com>
2019-02-20 11:24:11 +0100

    02

--- wooionicV4/src/app/home/home.page.ts
+++ wooionicV4/src/app/home/home.page.ts
@@ -12,11 +12,11 @@ export class HomePage {
  woocomm: any ;
  constructor(public nav: NavController) {
     this.woocomm = wc({
-      url: 'http://localhost/wooionicv.test/',
+      url: 'http://wooionicv.test/wp-json/',
       ConsumerKey: 'ck_5b879165f562fa8ed2c9c9bab6d6aae0209e4579',
       ConsumerSecret: 'cs_3044f208018523c58b8be94485bcb6ebe8a05e25',
       wpAPI: true,
-      version: 'wc/v3'
+     // version: 'wc/v3'
     });
     this.woocomm.getAsyn('products').then((data) => {
       console.log('nice');
