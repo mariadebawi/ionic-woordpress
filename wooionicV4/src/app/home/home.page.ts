e31332ef32ac01f37d91d9b3f834032fd9c39261
maria <debawimaria.fsm@gmail.com>
2019-02-25 11:03:49 +0100

    01

--- wooionicV4/src/app/home/home.page.ts
+++ wooionicV4/src/app/home/home.page.ts
@@ -12,7 +12,7 @@ export class HomePage {
   products: any[];
  constructor(public nav: NavController) {
     this.Woocommerce = WC({
-      url: 'https://artizone.tn/wp-json/wp/v2/product',
+      url: 'https://artizone.tn/' ,
       ConsumerKey: 'ck_c52c9af62a03a61cab5df7d61e62587688f1cd38',
       ConsumerSecret: 'cs_5d9da923a3136759b7c1f731a7cf71715678cecc',
     });
